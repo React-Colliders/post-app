199339f89952b21b5c2b78bc0b61743264678e6a
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-31 21:48:23 +0530

    post

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,12 @@
+export const POST_FETCHING = 'POST_FETCHING';
+export const POST_FETCH_SUCCESS = 'POST_FETCH_SUCCESS';
+export const POST_FETCH_ERROR = 'POST_FETCH_ERROR';
+
+
+
+
+// let initialState = {
+//     loading:  false,
+//     data: [],
+//     error: null
+// }
